a497d552ef4ab2815274e18d04953a7a5747e6fe
Eric Creager <eric.creager@yahoo.com>
2021-02-21 15:09:49 -0600

    should have created clear button, new button, and grid container using DOM

--- Etch.js
+++ Etch.js
@@ -1,1 +1,6 @@
 //this is the JS code for the etch-a-sketch project
+const clearButton = document.querySelector('.clearButton');
+const newButton = document.querySelector('.newEtch');
+const gridcontainer = document.querySelector('.gridContainer')
+const buttonColor = document.querySelector('.buttonColor')
+let color = 'black'
